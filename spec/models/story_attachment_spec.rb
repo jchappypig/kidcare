12a0e32930d7b02522baba2f3800b81928290da6
Huanhuan Huang <hhuang@thoughtworks.com>
2014-07-03 17:22:31 +1000

    forgot to commit spec file

--- spec/models/story_attachment_spec.rb
+++ spec/models/story_attachment_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe StoryAttachment do
-  it {is_expected.to belong_to :story}
-  it {is_expected.to validate_presence_of :photo}
-  it {is_expected.to validate_presence_of :story}
+  it { is_expected.to belong_to :story }
+  it { is_expected.to validate_presence_of :photo }
+  it { is_expected.to validate_presence_of :story }
 end
