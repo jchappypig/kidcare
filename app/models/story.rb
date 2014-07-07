594bc0441bf5134515419c289eac64bd7365822e
Huanhuan Huang <hhuang@thoughtworks.com>
2014-07-08 09:15:18 +1000

    destory story would also destory attachements

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,5 @@
 class Story < ActiveRecord::Base
-  has_many :story_attachment
+  has_many :story_attachment, dependent: :destroy
   accepts_nested_attributes_for :story_attachment
   validates :content, presence: true
 end
--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 describe Story do
-  it {is_expected.to have_many :story_attachment}
+  it {is_expected.to have_many(:story_attachment).dependent(:destroy)}
   it {is_expected.to validate_presence_of :content}
 end
