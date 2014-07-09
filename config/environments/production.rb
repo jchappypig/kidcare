4c6836f0b7745d9f17842ff4e005bc059113fa1c
Huanhuan Huang <hhuang@thoughtworks.com>
2014-07-10 07:03:57 +1000

    added additional precompile assets path and type.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
@@ -60,6 +60,9 @@ Rails.application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.paths << Rails.root.join('app', 'assets', 'foundation-icons')
+  config.assets.paths << Rails.root.join('app', 'assets', 'foundation-icons', 'svgs')
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
