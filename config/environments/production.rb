1fffaf328e757ff5cdde17ca64eb7b0f38a5b9fb
Huanhuan Huang <hhuang@thoughtworks.com>
2014-07-10 06:56:39 +1000

    Revert "font icon not working in production. try again."
    This reverts commit 66365f03f1ce26db944e46ef7d27bdd82327eeae.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass
-  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+  # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
