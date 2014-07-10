082c59abb06437d3bac879044030e0122ce5cd63
Huanhuan Huang <hhuang@thoughtworks.com>
2014-07-10 10:40:01 +1000

    added contact us page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,7 @@ class ApplicationController < ActionController::Base
 
   def index
   end
+
+  def contact_us
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'application#index'
+  get '/contact_us' => 'application#contact_us'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
