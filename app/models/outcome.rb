1c639a721d08bb669e45ed9d9a23863ddb6ba2f9
Huanhuan Huang <hhuang@thoughtworks.com>
2014-07-14 15:59:00 +1000

    display story description with item no.

--- app/models/outcome.rb
+++ app/models/outcome.rb
@@ -4,4 +4,8 @@ class Outcome < ActiveRecord::Base
   validates :category, presence: true
   has_many :story_outcomes
   has_many :stories, through: :story_outcomes
+
+  def long_description
+    return [item_no, description].join(' ')
+  end
 end
--- spec/models/outcome_spec.rb
+++ spec/models/outcome_spec.rb
@@ -6,4 +6,15 @@ describe Outcome do
   it { is_expected.to validate_presence_of :category }
   it { is_expected.to have_many :story_outcomes}
   it { is_expected.to have_many(:stories).through(:story_outcomes)}
+
+  describe 'long description' do
+    it 'returns item no and description' do
+      item_no = '1.1';
+      description = Faker::Lorem.sentence
+      outcome = Outcome.new(item_no: item_no, description: description)
+
+      expect(outcome.long_description).to include(item_no)
+      expect(outcome.long_description).to include(description)
+    end
+  end
 end
