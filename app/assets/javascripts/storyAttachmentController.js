d77db18a9f78c9fcf10a028a50fb01fef241b083
Huanhuan Huang <hhuang@thoughtworks.com>
2014-07-21 15:53:20 +1000

    query attachments again after delete.

--- app/assets/javascripts/storyAttachmentController.js
+++ app/assets/javascripts/storyAttachmentController.js
@@ -14,14 +14,21 @@ app.factory('_', ['$window', function($window) {
 }]);
 
 app.controller('StoryAttachmentController', ['StoryAttachment', 'StoryAttachments', '$scope', '_', function(StoryAttachment, StoryAttachments, $scope, _) {
+  var storyId;
+
+  var setAttachments = function() {
+    $scope.attachments = StoryAttachments.query({story_id: storyId});
+  };
+
   $scope.getAttachments = function(story_id) {
-    $scope.attachments = StoryAttachments.query({story_id: story_id});
+    storyId = story_id;
+    setAttachments();
   };
 
   $scope.removeAttachment = function(attachment) {
     if (confirm("Are you sure?")) {
       StoryAttachment.remove({id: attachment.id});
-      $scope.attachments = _.without($scope.attachments, attachment);
+      setAttachments();
     }
   };
 }]);
