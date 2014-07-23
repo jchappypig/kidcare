ba33afa9ac465265c6db683dbec7cd4db5d3cdde
Huanhuan Huang <hhuang@thoughtworks.com>
2014-07-23 19:32:19 +1000

    fix refresh attachment list bouncing issue.

--- app/assets/javascripts/angularjs/controllers/storyAttachmentController.js
+++ app/assets/javascripts/angularjs/controllers/storyAttachmentController.js
@@ -1,19 +1,28 @@
 App.controller('StoryAttachmentController', ['StoryAttachment', 'StoryAttachments', '$scope', '_', function(StoryAttachment, StoryAttachments, $scope, _) {
   var storyId;
 
-  var setAttachments = function() {
-    $scope.attachments = StoryAttachments.query({story_id: storyId});
-  };
-
   $scope.getAttachments = function(story_id) {
     storyId = story_id;
-    setAttachments();
+    $scope.attachments = StoryAttachments.query({story_id: storyId});
   };
 
   $scope.removeAttachment = function(attachment) {
     if (confirm("Are you sure?")) {
+      $scope.attachments = _.filter($scope.attachments, function(_attachment) {
+        return _attachment.id !== attachment.id
+      });
       StoryAttachment.remove({id: attachment.id});
-      setAttachments();
     };
   };
+
+  $scope.refreshAttachments = function() {
+    var newAttachments = StoryAttachments.query({story_id: storyId}, function() {
+      var currentAttachments = $scope.attachments;
+      _.each(newAttachments, function(newAttachment) {
+        if(_.where(currentAttachments, {id: newAttachment.id}).length == 0) {
+          $scope.attachments.push(newAttachment);
+        };
+      });
+    });
+  }
 }]);
