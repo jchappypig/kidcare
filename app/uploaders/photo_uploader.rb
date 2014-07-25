0b3992678b0abf5501dbc5d7aea61180a04f261b
Huanhuan Huang <hhuang@thoughtworks.com>
2014-07-25 17:08:36 +1000

    styling our stories page and have outcomes.

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -19,7 +19,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   end
 
   version :thumb do
-    process :resize_to_fill => [250,250]
+    process :resize_to_fill => [270,270]
   end
 
   version :preview do
