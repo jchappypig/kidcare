4e356f13f534d37d72955dd4620709cb243bce90
Huanhuan Huang <hhuang@thoughtworks.com>
2014-08-14 18:12:55 +1000

    remove the require from application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,10 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# dotenv
-require 'dotenv'
-Dotenv.load
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
