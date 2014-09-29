a15fbec71cdbc7d00198423cc4faf43e9a33967c
Huanhuan Huang <hhuang@thoughtworks.com>
2014-09-29 11:02:32 +1000

    Revert "fix photo cannot be blank on firefox"
    This reverts commit 18cc4ad9eb77c7ce617204a054af27c156734a8c.

--- app/models/story_attachment.rb
+++ app/models/story_attachment.rb
@@ -1,4 +1,5 @@
 class StoryAttachment < ActiveRecord::Base
   mount_uploader :photo, PhotoUploader
   belongs_to :story
+  validates :photo, presence: true
 end
--- spec/models/story_attachment_spec.rb
+++ spec/models/story_attachment_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe StoryAttachment do
   it { is_expected.to belong_to :story }
+  it { is_expected.to validate_presence_of :photo }
 end
