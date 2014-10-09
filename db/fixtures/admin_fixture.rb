e6c043ec5c2886bd365e363b9ccb418ad6e8cc5a
Huanhuan Huang <hhuang@thoughtworks.com>
2014-10-10 08:12:54 +1100

    added another admin

--- db/fixtures/admin_fixture.rb
+++ db/fixtures/admin_fixture.rb
@@ -1,2 +1,3 @@
 User.seed(:id,
-          {id: 1, admin: true, email: 'jchappypig@hotmail.com', password: '12341234'});
+          {id: 1, admin: true, email: 'jchappypig@hotmail.com', password: '12341234'},
+          {id: 2, admin: true, email: 'littlestarelc@bigpond.com', password: '12341234'});
